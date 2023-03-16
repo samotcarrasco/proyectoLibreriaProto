8d9e54d42c7cd649816fd4261e0920590f30f57a
santiago galan <santiago.galan@gmail.com>
2023-03-16 12:08:25 +0100

    primer commit LIBRERIA

---
+++ src/main/java/es/mde/acing/proyectoLibProto/ProyectoLibProtoApplication.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibProto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLibProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLibProtoApplication.class, args);
+	}
+
+}
