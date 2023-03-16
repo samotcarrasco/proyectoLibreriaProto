016b4e9358ac8157a6d8fa15d528e8c7d355815c
santiago galan <santiago.galan@gmail.com>
2023-03-16 12:28:01 +0100

    Actualizacion libreria

--- src/main/java/es/mde/acing/proyectoLibProto/ProyectoLibProtoApplication.java
+++ src/main/java/es/mde/acing/proyectoLibProto/ProyectoLibProtoApplication.java
@@ -1,13 +1,10 @@
 package es.mde.acing.proyectoLibProto;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class ProyectoLibProtoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoLibProtoApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/es/mde/acing/proyectoLibProto/utils/PruebaLibreria.java
@@ -0,0 +1,8 @@
+package es.mde.acing.proyectoLibProto.utils;
+
+public class PruebaLibreria {
+
+	public static void imprime() {
+		System.out.println("Hola librería!");
+	}
+}
